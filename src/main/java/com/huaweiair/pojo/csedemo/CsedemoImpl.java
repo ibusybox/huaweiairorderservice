f05268b3fe3321d219a9cf11ded9f10992da60f8
paas <paas@cse-codegen-backend-1592449050-pkkb5>
2017-11-08 07:23:11 +0000

    Auto pushed by FusionStage!

--- src/main/java/com/huaweiair/pojo/csedemo/CsedemoImpl.java
+++ src/main/java/com/huaweiair/pojo/csedemo/CsedemoImpl.java
@@ -7,7 +7,7 @@ import javax.validation.constraints.*;
 import io.servicecomb.provider.pojo.RpcSchema;
 import com.huaweiair.pojo.csedemo.Csedemo;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.CsePojoCodegen", date = "2017-11-08T07:06:59.246Z")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.CsePojoCodegen", date = "2017-11-08T07:23:11.823Z")
 
 @RpcSchema(schemaId = "csedemo")
 public class CsedemoImpl implements Csedemo {
--- src/main/java/com/huaweiair/pojo/csedemo/model/Person.java
+++ src/main/java/com/huaweiair/pojo/csedemo/model/Person.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import javax.validation.constraints.*;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.CsePojoCodegen", date = "2017-11-08T07:06:59.246Z")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.CsePojoCodegen", date = "2017-11-08T07:23:11.823Z")
 
 public class Person   {
   @JsonProperty("name")
